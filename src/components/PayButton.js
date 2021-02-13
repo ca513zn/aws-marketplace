8cb925f54ea4ee81e182af7bf71ca2d0b815f417
ca513zn <ca513zn@gmail.com>
2021-02-13 12:36:57 -0600

    completing lesson 46

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,3 +1,4 @@
+import { API } from "aws-amplify";
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 // import { Notification, Message } from "element-react";
@@ -7,8 +8,21 @@ const stripeConfig = {
     "pk_test_51HTAdWBKTHkPcr71DpJW6kLoehXXxpyGg68uLvuSM6TXMzNRsyAArB5LoRevSlLgGAHl5RdzuSNvMWhMBJJk9i5m00QdaszmsX",
 };
 const PayButton = ({ product, user }) => {
+  const handleCharge = async (token) => {
+    try {
+      const result = await API.post("orderlambda", "/charge", {
+        body: {
+          token,
+        },
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <StripeCheckout
+      token={handleCharge}
       email={user.attributes.email}
       name={product.description}
       amount={product.price}
