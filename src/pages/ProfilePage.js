a5f9f201002b244c2bdf2f8622cbb15038529011
ca513zn <ca513zn@gmail.com>
2021-02-14 09:44:03 -0600

    completing lesson 57

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { API, graphqlOperation } from "aws-amplify";
+import { Auth, API, graphqlOperation } from "aws-amplify";
 // prettier-ignore
 import { Table, Button, Notification, MessageBox, Message, Tabs, Icon, Form, Dialog, Input, Card, Tag } from 'element-react'
 import { converCentsToDollars } from "../utils";
+import useDialog from "../hooks/useDialog";
 const getUser = `
 query GetUser($id: ID!) {
   getUser(id: $id) {
@@ -38,6 +39,8 @@ query GetUser($id: ID!) {
 }
 `;
 const ProfilePage = ({ user, userAttributes }) => {
+  const emailDialog = useDialog();
+  const verificationForm = useDialog();
   const [state, setState] = useState({
     orders: [],
     columns: [
@@ -63,7 +66,11 @@ const ProfilePage = ({ user, userAttributes }) => {
           switch (row.name) {
             case "Email":
               return (
-                <Button type="info" size="small">
+                <Button
+                  onClick={emailDialog.handleOpen}
+                  type="info"
+                  size="small"
+                >
                   Edit
                 </Button>
               );
@@ -79,6 +86,8 @@ const ProfilePage = ({ user, userAttributes }) => {
         },
       },
     ],
+    email: userAttributes.email && userAttributes.email,
+    verificationCode: "",
   });
   useEffect(() => {
     if (userAttributes) {
@@ -97,93 +106,191 @@ const ProfilePage = ({ user, userAttributes }) => {
       console.log(error);
     }
   };
-  return userAttributes && (
-    <>
-      <Tabs activeName="1" className="profile-tabs">
-        <Tabs.Pane
-          name="1"
-          label={
-            <>
-              <Icon name="document" className="icon" />
-              Summary
-            </>
-          }
-        >
-          <h2 className="header">Profile Summary</h2>
-          <Table
-            columns={state.columns}
-            data={[
-              {
-                name: "Your Id",
-                value: userAttributes.sub,
-              },
-              {
-                name: "Username",
-                value: user.username,
-              },
-              {
-                name: "Email",
-                value: userAttributes.email,
-              },
-              {
-                name: "Phone Number",
-                value: userAttributes.phone_number,
-              },
-              {
-                name: "Delete Profile",
-                value: "Sorry to see you go...",
-              },
-            ]}
-            showHeader={false}
-            rowClassName={(row) =>
-              row.name === "Delete Profile" && "delete-profile"
+  const handleUpdateEmail = async () => {
+    try {
+      const updatedAttributes = {
+        email: state.email,
+      };
+      const result = await Auth.updateUserAttributes(user, updatedAttributes);
+      if (result === "SUCCESS") {
+        sendVerificationCode("email");
+      }
+    } catch (error) {
+      console.error(error);
+      Notification.error({
+        title: "Error",
+        message: "There was an error updating your email.",
+      });
+    }
+  };
+
+  const sendVerificationCode = async (attr) => {
+    await Auth.verifyCurrentUserAttribute(attr);
+    verificationForm.handleOpen();
+    Message({
+      type: "info",
+      customClass: "message",
+      message: `Verification code sent to ${state.email}`,
+    });
+  };
+
+  const handleVerifyEmail = async (attr) => {
+    try {
+      const result = await Auth.verifyCurrentUserAttributeSubmit(
+        attr,
+        state.verificationCode
+      );
+      Notification({
+        title: "success",
+        message: "Email successfully verified",
+        type: `${result.toLowerCase()}`,
+      });
+      setTimeout(() => window.location.reload(), 3000);
+    } catch (error) {
+      console.log(error);
+      Notification.error({
+        title: "Error",
+        message: `${error.message || "Error Verifying email."}`,
+      });
+    }
+  };
+
+  return (
+    userAttributes && (
+      <>
+        <Tabs activeName="1" className="profile-tabs">
+          <Tabs.Pane
+            name="1"
+            label={
+              <>
+                <Icon name="document" className="icon" />
+                Summary
+              </>
             }
-          />
-        </Tabs.Pane>
-        <Tabs.Pane
-          name="2"
-          label={
-            <>
-              <Icon name="message" className="icon" />
-              Orders
-            </>
-          }
+          >
+            <h2 className="header">Profile Summary</h2>
+            <Table
+              columns={state.columns}
+              data={[
+                {
+                  name: "Your Id",
+                  value: userAttributes.sub,
+                },
+                {
+                  name: "Username",
+                  value: user.username,
+                },
+                {
+                  name: "Email",
+                  value: userAttributes.email,
+                },
+                {
+                  name: "Phone Number",
+                  value: userAttributes.phone_number,
+                },
+                {
+                  name: "Delete Profile",
+                  value: "Sorry to see you go...",
+                },
+              ]}
+              showHeader={false}
+              rowClassName={(row) =>
+                row.name === "Delete Profile" && "delete-profile"
+              }
+            />
+          </Tabs.Pane>
+          <Tabs.Pane
+            name="2"
+            label={
+              <>
+                <Icon name="message" className="icon" />
+                Orders
+              </>
+            }
+          >
+            <h2 className="header">Order History</h2>
+            {state.orders.map((order) => {
+              console.log(order);
+              return (
+                <div className="mb-1" key={order.id}>
+                  <Card>
+                    <pre>
+                      <p>Order Id: {order.id}</p>
+                      <p>Product Description: {order.product.description}</p>
+                      <p>
+                        Price: ${converCentsToDollars(order.product.price)}{" "}
+                        (USD)
+                      </p>
+                      <p>Purchased on: {order.createdAt}</p>
+                      {order.shippingAddress && (
+                        <>
+                          Shipping Address:
+                          <div className="ml-2">
+                            <p>{order.shippingAddress.address_line1}</p>
+                            <p>
+                              {order.shippingAddress.city},{" "}
+                              {order.shippingAddress.address_state}{" "}
+                              {order.shippingAddress.country}{" "}
+                              {order.shippingAddress.address_zip}
+                            </p>
+                          </div>
+                        </>
+                      )}
+                    </pre>
+                  </Card>
+                </div>
+              );
+            })}
+          </Tabs.Pane>
+        </Tabs>
+        <Dialog
+          size="large"
+          customClass="dialog"
+          title="Edit Email"
+          visible={emailDialog.open}
+          onCancel={emailDialog.handleClose}
         >
-          <h2 className="header">Order History</h2>
-          {state.orders.map((order) => {
-            console.log(order);
-            return (
-              <div className="mb-1" key={order.id}>
-                <Card>
-                  <pre>
-                    <p>Order Id: {order.id}</p>
-                    <p>Product Description: {order.product.description}</p>
-                    <p>
-                      Price: ${converCentsToDollars(order.product.price)} (USD)
-                    </p>
-                    <p>Purchased on: {order.createdAt}</p>
-                    {order.shippingAddress && (
-                      <>
-                        Shipping Address:
-                        <div className="ml-2">
-                          <p>{order.shippingAddress.address_line1}</p>
-                          <p>
-                            {order.shippingAddress.city},{" "}
-                            {order.shippingAddress.address_state}{" "}
-                            {order.shippingAddress.country}{" "}
-                            {order.shippingAddress.address_zip}
-                          </p>
-                        </div>
-                      </>
-                    )}
-                  </pre>
-                </Card>
-              </div>
-            );
-          })}
-        </Tabs.Pane>
-      </Tabs>
-    </>
+          <Dialog.Body>
+            <Form labelPosition="top">
+              <Form.Item label="Email">
+                <Input
+                  value={state.email}
+                  onChange={(val) =>
+                    setState((prevState) => ({ ...prevState, email: val }))
+                  }
+                />
+              </Form.Item>
+              {verificationForm.open && (
+                <Form.Item label="Enter Verification Code" labelWidth="120">
+                  <Input
+                    value={state.verificationCode}
+                    onChange={(val) =>
+                      setState((prevState) => ({
+                        ...prevState,
+                        verificationCode: val,
+                      }))
+                    }
+                  />
+                </Form.Item>
+              )}
+            </Form>
+          </Dialog.Body>
+          <Dialog.Footer>
+            <Button onClick={emailDialog.handleClose}>Cancel</Button>
+            {!verificationForm.open && (
+              <Button type="primary" onClick={handleUpdateEmail}>
+                Save
+              </Button>
+            )}
+            {verificationForm.open && (
+              <Button type="primary" onClick={() => handleVerifyEmail("email")}>
+                Submit
+              </Button>
+            )}
+          </Dialog.Footer>
+        </Dialog>
+      </>
+    )
   );
 };
 
